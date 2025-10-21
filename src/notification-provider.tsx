86624bfc2c6af69774d443a54347078c3afc91af
raktaxetu <raktaxetu.tech@gmail.com>
2025-10-22 00:15:50 +0530

    added external ids

--- src/notification-provider.tsx
+++ src/notification-provider.tsx
@@ -2,8 +2,10 @@
 
 import { useEffect } from "react";
 import OneSignal from "react-onesignal";
+import { authClient } from "./lib/auth-client";
 
 export function NotificationProvider({ children }: Children) {
+  const { data: session } = authClient.useSession();
   useEffect(() => {
     if (typeof window !== "undefined") {
       OneSignal.init({
@@ -14,5 +16,11 @@ export function NotificationProvider({ children }: Children) {
       });
     }
   }, []);
+
+  useEffect(() => {
+    if (session?.user.id) {
+      OneSignal.login(session.user.id);
+    }
+  }, [session?.user.id]);
   return <>{children}</>;
 }
