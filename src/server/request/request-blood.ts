57d07b20d361589d3758ed69cdb0d53340af81ca
raktaxetu <raktaxetu.tech@gmail.com>
2025-10-22 19:53:02 +0530

    updated notification strategy for requesting blood

--- src/server/request/request-blood.ts
+++ src/server/request/request-blood.ts
@@ -23,11 +23,12 @@ export const requestBlood = async (request: IBlood) => {
         "https://onesignal.com/api/v1/notifications",
         {
           app_id: process.env.ONESIGNAL_APP_ID!,
-          included_segments: ["All"],
+          exclude_external_user_ids: [session.user.id],
           headings: { en: "New Blood Request!" },
           contents: {
             en: `A new blood request for ${request.bloodGroup} has been submitted at ${request.location}. Please help if you can!`,
           },
+          url: `${process.env.BETTER_AUTH_URL!}/request-blood`,
         },
         {
           headers: {
