684e50fafc99021291d44da82749187db897c457
raktaxetu <raktaxetu.tech@gmail.com>
2025-10-26 09:19:01 +0530

    modified donors list to handle ai results

--- src/components/modules/donors/ui/search.tsx
+++ src/components/modules/donors/ui/search.tsx
@@ -2,34 +2,50 @@
 
 import { searchDonorsWithAI } from "@/vector/services/search-vectors";
 import { HoverBorderGradient } from "@/components/ui/hover-border-gradient";
-import { useState } from "react";
 import Image from "next/image";
+import { useAISearch } from "@/store/search-ai";
+import { cn } from "@/lib/utils";
+import { Shimmer } from "@/components/ai-elements/shimmer";
+import { useAIMatch } from "@/store/match";
 
 export function AISearch() {
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const { isAISearching, setIsAISearching } = useAISearch();
+  const { setMatches } = useAIMatch();
   const search = async () => {
     try {
-      setIsLoading(true);
+      setIsAISearching(true);
       const results = await searchDonorsWithAI();
-      console.log(results);
+      setMatches(results);
     } catch (error) {
       console.error(error);
     } finally {
-      setIsLoading(false);
+      setIsAISearching(false);
     }
   };
   return (
     <div onClick={search}>
       <HoverBorderGradient
-        disabled={isLoading}
+        disabled={isAISearching}
         containerClassName="rounded-full"
         as="button"
-        className="dark:bg-black bg-white text-black cursor-pointer dark:text-white flex items-center space-x-2"
+        className={cn(
+          "dark:bg-black bg-white text-black cursor-pointer dark:text-white flex items-center space-x-2"
+        )}
       >
-        <Image src="/gemini.png" height={16} width={16} alt="google logo" />
-        <span className="text-neutral-500 text-sm font-light">
-          Search With AI
-        </span>
+        <Image
+          src="/gemini.png"
+          height={16}
+          width={16}
+          alt="google logo"
+          className={cn(isAISearching && "animate-spin-with-pause")}
+        />
+        {isAISearching ? (
+          <Shimmer className="text-sm font-light">Searching...</Shimmer>
+        ) : (
+          <span className="text-neutral-500 text-sm font-light">
+            Search With AI
+          </span>
+        )}
       </HoverBorderGradient>
     </div>
   );
