570d942ed7ef37514bdcba0e223aae511e318828
raktaxetu <raktaxetu.tech@gmail.com>
2025-11-02 11:08:03 +0530

    updated scripts to upsert vector

--- src/vector/services/donor-vectors.ts
+++ src/vector/services/donor-vectors.ts
@@ -5,6 +5,10 @@ export const upsertVector = async (profile: IProfile) => {
   const record = {
     id: profile._id?.toString()!,
     text: `${profile.name}, Blood Group: ${profile.bloodGroup}, Location: ${profile.location}, DOB: ${profile.dateOfBirth}`,
+    bloodGroup: profile.bloodGroup,
+    location: profile.location,
+    name: profile.name,
+    dateOfBirth: profile.dateOfBirth,
   };
   await index.upsertRecords([record]);
 };
